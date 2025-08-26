bbfc0064d5588e0faf313b6f1190d0ace1d9703e
F0RT-DEV <douglasf935255@gmail.com>
2025-08-26 15:29:22 -0300

    removendo os imports do db referentes ao firebase

--- Backend_Amapa/src/db.js
+++ Backend_Amapa/src/db.js
@@ -1,15 +1,2 @@
-import admin from 'firebase-admin';
-import { getFirestore } from 'firebase-admin/firestore';
-
-import { createRequire } from 'node:module';
-const require = createRequire(import.meta.url);
-const serviceAccount = require('./serviceAccountKey.json');
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-}
-
-const db = getFirestore();
+import { db } from './firebase.js';
 export { db };
